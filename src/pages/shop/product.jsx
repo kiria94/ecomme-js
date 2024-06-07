d88aa3aae75d56ac65dcf92436ef9524bd251383
Ciro <chirilovcinicov@gmail.com>
2024-06-07 12:25:08 +0200

    first commit

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+
+export const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  //let's take the amount in memory
+  const cartItemAmount = cartItems[id];
+  return (
+    <div className="product">
+      <img src={productImage} alt="#" />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p>${price}</p>
+      </div>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add to Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}
+      </button>
+    </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,60 @@
+//faking a json data we're goint to be using
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.png";
+import product3 from "./assets/3.png";
+import product4 from "./assets/4.jpg";
+import product5 from "./assets/5.jpg";
+import product6 from "./assets/6.webp";
+import product7 from "./assets/7.webp";
+import product8 from "./assets//8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon M50 Camera",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "WLS VAN Gogh Donim Jacket",
+    price: 999.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "LED Light Strips",
+    price: 199.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Spectrum LS TEE",
+    price: 69.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "AUTO SERVICE SHIRT by GOLD WANG",
+    price: 66.9,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "DON'T PANIC, TRIP STRUCTURED",
+    price: 96.69,
+    productImage: product8,
+  },
+];
